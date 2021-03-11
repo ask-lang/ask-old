1e48dc7794e3df660e303ac5670f7bde77da1743
Liao.Peng <pengliao@live.cn>
2021-03-11 12:12:14 +0800

    feature: add type 128 support and mutates support
    Signed-off-by: Liao.Peng <pengliao@live.cn>

--- examples/solar/solar.ts
+++ examples/solar/solar.ts
@@ -2,12 +2,14 @@
  * All Rights Reserved by Patract Labs.
  * @author liangqin.fan@gmail.com
  */
+import {u128} from "../../assembly"
 
 @storage
 class Planets {
   name: string;
   radius: u32;
   isdwarf: boolean;
+  typeU128: u128;
 }
 
 @contract
@@ -19,7 +21,7 @@ class SolarSystem {
   }
 
   @constructor
-  default(name: string = "Earth", radius: u32 = 6300, isdwarf: boolean = false): void {
+  default(name: string = "Earth", radius: u32 = 6300, isdwarf: boolean = false, type128: u128 = u128.fromU64(12)): void {
     this.stored.name = name;
     this.stored.radius = radius;
     this.stored.isdwarf = isdwarf;
@@ -31,11 +33,29 @@ class SolarSystem {
       this.stored.name = name;
       this.stored.radius = radius;
       this.stored.isdwarf = isdwarf;
+      this.stored.typeU128 = u128.fromU64(32);
     }
+  }
+
+  @message(mutates = false)
+  getType128(): u128 {
+    return this.stored.typeU128;
+  }
+
+  @message
+  setType128(type128: u128): void {
+    this.stored.typeU128 = type128;
   }
 
   @message(mutates = false, selector = "0x0a0b0c0d")
   getRadius(): u32 {
     return this.stored.radius;
   }
+
+  @message(mutates = false)
+  errorGetRadius(): u32 {
+    this.stored.radius = 100;
+    return this.stored.radius;
+  }
+
 }
